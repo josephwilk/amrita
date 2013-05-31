9481dbfb6148a569ada7ac59323ed10e71ceaee0
Joseph Wilk <joe@josephwilk.net>
2013-06-01 00:03:10 +0200

    adding mix package file

---
+++ mix.exs
@@ -0,0 +1,17 @@
+defmodule Amrita.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :amrita,
+     version: "0.1.0",
+     deps: deps]
+  end
+
+  def application do
+    [applications: []]
+  end
+
+  defp deps do
+    []
+  end
+end
