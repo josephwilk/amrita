1902c283478a332ec491240ad57953a231e61483
josephwilk <joe@josephwilk.net>
2013-06-05 19:19:00 +0200

    more tags

--- package.exs
+++ package.exs
@@ -2,6 +2,6 @@ Expm.Package.new(name:        "amrita",
                  description: "A polite, well mannered and throughly upstanding testing framework for Elixir",
                  homepage:    "http://github.com/josephwilk/amrita",
                  version:     "0.1.1",
-                 keywords:     ["testing", "tdd", "bdd"],
+                 keywords:     ["testing", "tdd", "bdd", "elixir"],
                  maintainers:  [[name: "Joseph Wilk", email: "joe@josephwilk.net"]],
                  repositories: [[github: "josephwilk/amrita"]])
