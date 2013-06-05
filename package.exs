8b782acd203e486f9019a1fc6519307830b65958
josephwilk <joe@josephwilk.net>
2013-06-05 19:13:03 +0200

    typo

--- package.exs
+++ package.exs
@@ -1,6 +1,6 @@
 Expm.Package.new(name:        "amrita",
                  description: "A polite, well mannered and throughly upstanding testing framework for Elixir",
-                 homepage:    "http://github.com/josephwilk/amrita"
+                 homepage:    "http://github.com/josephwilk/amrita",
                  version:     "0.1.1",
                  keywords:     ["testing", "tdd", "bdd"],
                  maintainers:  [[name: "Joseph Wilk", email: "joe@josephwilk.net"]],
