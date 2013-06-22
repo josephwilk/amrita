a622e053cbf7b0c85a26494510125cbcf04db28a
Joseph Wilk <joe@josephwilk.net>
2013-06-22 10:45:43 +0200

    Working towards supporting +1 prerequisites against different modules

--- lib/amrita/mocks.ex
+++ lib/amrita/mocks.ex
@@ -8,21 +8,31 @@ defmodule Amrita.Mocks do
 
   defmodule Provided do
     defmacro provided(forms, test) do
-      { mock_module, fn_name, value } =  Amrita.Mocks.ParsePrerequisites.module_fn(Enum.at(forms, 0))
+      prerequisites = Amrita.Mocks.ParsePrerequisites.prerequisites(forms)
+      mock_modules = Dict.keys(prerequisites)
+
+      { :ok, [{ _, fn_name, value } | _] } = Dict.fetch(prerequisites, Enum.at(mock_modules,0))
 
       quote do
-        :meck.new(unquote(mock_module), [:passthrough])
-        unquote(__MODULE__).__add_expect__(unquote(mock_module), unquote(fn_name), unquote(value))
+        Enum.map unquote(mock_modules), fn mock_module ->
+          :meck.new(mock_module, [:passthrough])
+          unquote(__MODULE__).__add_expect__(mock_module, unquote(fn_name), unquote(value))
+        end
 
         try do
           unquote(test)
-          :meck.validate(unquote(mock_module)) |> truthy
-        after
-          r = :meck.called(unquote(mock_module), unquote(fn_name), :_)
-          :meck.unload(unquote(mock_module))
 
-          if not(r), do: Amrita.Message.fail "#{unquote(fn_name)} called 0 times",
-                                             "expected at least once", {"called", ""}
+          Enum.map unquote(mock_modules), fn mock_module ->
+            :meck.validate(mock_module) |> truthy
+          end
+        after
+          Enum.map unquote(mock_modules), fn mock_module ->
+            r = :meck.called(mock_module, unquote(fn_name), :_)
+            :meck.unload(mock_module)
+
+            if not(r), do: Amrita.Message.fail "#{unquote(fn_name)} called 0 times",
+                                               "expected at least once", {"called", ""}
+          end
         end
       end
     end
@@ -34,17 +44,26 @@ defmodule Amrita.Mocks do
   end
 
   defmodule ParsePrerequisites do
-    def module_fn({:|>, _, [{l, _, _}, v]}) do
+    def prerequisites(forms) do
+      prerequisites = Enum.map(forms, fn form -> module_fn(form) end)
+      prerequisites = Enum.reduce prerequisites, HashDict.new, fn {m,f,v}, acc ->
+        mocks = HashDict.get(acc, m, [])
+        mocks = List.concat(mocks, [{m,f,v}])
+        HashDict.put(acc,m,mocks)
+      end
+    end
+
+    defp module_fn({:|>, _, [{l, _, _}, v]}) do
       { module_name, function_name } = module_fn(l)
       { module_name, function_name,  v }
     end
 
-    def module_fn({:., _, [ns, method_name]}) do
+    defp module_fn({:., _, [ns, method_name]}) do
       { module_fn(ns), method_name }
     end
 
-    def module_fn({:__aliases__, _, ns}) do
-      Module.concat(ns)
+    defp module_fn({:__aliases__, _, ns}) do
+      Module.concat ns
     end
   end
 
