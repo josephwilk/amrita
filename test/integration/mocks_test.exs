d51aa04dc6ae9564f89bafca39ff797925d233c6
Joseph Wilk <joe@josephwilk.net>
2013-07-14 17:44:53 +0200

    Example for private functions used in a mock

--- test/integration/mocks_test.exs
+++ test/integration/mocks_test.exs
@@ -192,6 +192,16 @@ defmodule MocksTest do
       end
     end
 
+    defp drink do
+      :sherry
+    end
+
+    future_fact "mock with a private function defined inside a test" do
+      provided [MocksTest.Funk.hip?(drink) |> false] do
+        Funk.hip?(:sherry) |> falsey
+      end
+    end
+
     fact "mock with many arguments" do
       provided [MocksTest.Funk.flop?(:yes, :no, :yes) |> false] do
         Funk.flop?(:yes, :no, :yes) |> falsey
