12d1a6d0a023e44bea1adc679616fa0fc4352771
Joseph Wilk <joe@josephwilk.net>
2013-07-25 21:45:03 +0200

    Playing with scoping rules of facts blocks.

---
+++ test/integration/scoping_test.exs
@@ -0,0 +1,25 @@
+Code.require_file "../test_helper.exs", __DIR__
+
+defmodule ScopingFacts do
+  use Amrita.Sweet
+  import Support
+
+  def echo do
+    1
+  end
+  
+  fact "echo 1" do
+    echo |> 1
+  end
+  
+  facts "echo nested" do
+    def echo do
+      2
+    end
+    
+    future_fact "echo 2" do
+      echo |> 2
+    end
+  end
+
+end
