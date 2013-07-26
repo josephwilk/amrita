923ebaf9c5842fc23078cb0d26627b3733cd8807
josephwilk <joe@josephwilk.net>
2013-07-26 18:04:58 +0200

    One liner facts. In all the pain of todays fighting with scopes I realised I can write one liner facts.

--- test/integration/scoping_test.exs
+++ test/integration/scoping_test.exs
@@ -4,22 +4,14 @@ defmodule ScopingFacts do
   use Amrita.Sweet
   import Support
 
-  def echo do
-    1
-  end
-  
-  fact "echo 1" do
-    echo |> 1
-  end
+  def echo, do:   1
+
+  fact "echo 1", do: echo |> 1
   
   facts "echo nested" do
-    def echo do
-      2
-    end
+    def echo, do: 2
     
-    future_fact "echo 2" do
-      echo |> 2
-    end
+    future_fact "echo 2", do: echo |> 2
   end
 
 end
