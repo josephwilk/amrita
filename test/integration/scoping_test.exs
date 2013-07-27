dcdaa20b9dfd7a0d95839fdacab5a75a2c598867
Joseph Wilk <joe@josephwilk.net>
2013-07-27 21:13:39 +0200

    thinking about scoping rules.

--- test/integration/scoping_test.exs
+++ test/integration/scoping_test.exs
@@ -7,11 +7,31 @@ defmodule ScopingFacts do
   def echo, do:   1
 
   fact "echo 1", do: echo |> 1
-  
+
   facts "echo nested" do
     def echo, do: 2
-    
+
     future_fact "echo 2", do: echo |> 2
+  end
+
+  setup do
+    {:ok, ping: :pong}
+  end
+
+  facts "setup within facts" do
+    setup do
+      {:ok, pong: :ping}
+    end
+
+    fact "has access to parent and local setup", meta do
+      meta[:ping] |> :pong
+      meta[:pong] |> :ping
+    end
+  end
+
+  future_fact "parent should only have access to root setup", meta do
+    meta[:ping] |> :pong
+    meta[:pong] |> ! :ping
   end
 
 end
