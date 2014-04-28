60586cb49234c093b870226aadb5abb0b6dc91f9
Joseph Wilk <joe@josephwilk.net>
2014-04-28 19:41:04 +0200

    hex

--- mix.exs
+++ mix.exs
@@ -1,3 +1,5 @@
+Code.ensure_loaded?(Hex) and Hex.start
+
 defmodule Amrita.Mixfile do
   use Mix.Project
 
@@ -5,9 +7,14 @@ defmodule Amrita.Mixfile do
     [app: :amrita,
      version: version,
      name: "Amrita",
+     description: "A polite, well mannered and thoroughly upstanding testing framework for Elixir",
      source_url: "https://github.com/josephwilk/amrita",
      elixir: "~> 0.13.0",
      homepage_url: "http://amrita.io",
+     package: [links: [{"Website", "http://amrita.io"},
+                       {"Source", "http://github.com/josephwilk/amrita"}],
+              contributors: ["Joseph Wilk"],
+              licenses: ["MIT"]],
      env: [test: [deps: deps],
            dev:  [deps: deps ++ dev_deps]],
      deps: deps]
